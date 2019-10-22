1ab76a6a2854a9c78567cf5698075c249133cc2a
sasza <sasza@onet.eu>
2019-10-22 20:25:37 +0200

    Border refactor

--- src/index.js
+++ src/index.js
@@ -4,37 +4,61 @@ import ResizeDetector from 'react-resize-detector'
 
 import './border.css'
 
-const Border = ({ children, radius, stroke, strokeWidth, strokeDasharray }) => {
+const CLASS_NAME = 'react-border'
+
+const Border = ({ children, className, radius, stroke, strokeWidth, strokeDasharray }) => {
   const svgRef = useRef()
   const rectRef = useRef()
+
+  const update = (width, height) => {
+    if (!svgRef.current || !rectRef.current) return
+
+    svgRef.current.setAttribute('width', `${width}px`)
+    svgRef.current.setAttribute('height', `${height}px`)
+    rectRef.current.setAttribute('width', `${width - strokeWidth}px`)
+    rectRef.current.setAttribute('height', `${height - strokeWidth}px`)
+  }
+
+  const renderBorder = () => {
+    const style = {
+      fill: 'none',      
+    }
+
+    if (className === CLASS_NAME){
+      style.stroke = stroke
+      style['stroke-width'] = strokeWidth
+      style['stroke-dasharray'] = strokeDasharray
+    }
+
+    return (
+      <g className={`${className}__border`} style={style}>
+        <rect ref={rectRef} rx={radius} x={strokeWidth/2} y={strokeWidth/2} style={style}/>
+      </g>
+    )
+  } 
+
+  const renderBackground = () => {
+    const style = {
+      'pointer-events': 'none',
+    }
+    return (
+      <svg className={`${className}__bg`} ref={svgRef} style={style}>
+        {renderBorder()}
+      </svg>
+    )
+  }
+
   return (
-    <div className='react-border'>
+    <div className={className}>
       <ResizeDetector
         handleWidth
         handleHeight
         render={({ width, height }) => {
-          if (svgRef.current && rectRef.current){
-            svgRef.current.setAttribute('width', `${width}px`)
-            svgRef.current.setAttribute('height', `${height}px`)
-            rectRef.current.setAttribute('width', `${width - strokeWidth}px`)
-            rectRef.current.setAttribute('height', `${height - strokeWidth}px`)
-          }
-
-          const style = {
-            fill: 'none',
-            stroke,
-            'stroke-width': strokeWidth,
-            'stroke-dasharray': strokeDasharray
-          }
-
+          update(width, height)
           return (
            <Fragment>
               {children}
-              <svg className='react-border__in' ref={svgRef}>
-                <g className='react-border__stroke' style={style}>
-                  <rect ref={rectRef} rx={radius} x={strokeWidth/2} y={strokeWidth/2} style={{fill: 'none'}}/>
-                </g>
-              </svg>
+              {renderBackground()}
            </Fragment>
           )
         }}
@@ -45,10 +69,15 @@ const Border = ({ children, radius, stroke, strokeWidth, strokeDasharray }) => {
 
 Border.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
   radius: PropTypes.number,
   stroke: PropTypes.string,
   strokeWidth: PropTypes.number,
   strokeDasharray: PropTypes.string,    
+}
+
+Border.defaultTypes = {
+  className: CLASS_NAME,
 }
 
 export default Border
